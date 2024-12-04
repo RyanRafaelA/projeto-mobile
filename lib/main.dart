08b1304e6f36342de9a02ffac987872870919dcc
RyanRafaelA <ryrafael461@gmail.com>
2024-12-03 22:51:27 -0300

    Criação da tela de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,18 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool entrar = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: (entrar) ? Colors.black : Colors.brown[900],
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Colors.red,
-        title: const Text(
-          "Tela de Login",
-          style: TextStyle(
+        backgroundColor: (entrar) ? Colors.red : Colors.deepOrangeAccent,
+        title: Text(
+          (entrar) ? "Tela de Login" : "Tela de Cadastro",
+          style: const TextStyle(
             color: Colors.white,
             fontSize: 18,
             fontWeight: FontWeight.w500,
@@ -39,13 +41,45 @@ class _LoginState extends State<Login> {
           child: Form(
             child: Column(
               children: [
-                const Icon(
-                  Icons.account_circle_rounded,
+                Icon(
+                  (entrar)
+                      ? Icons.account_circle_rounded
+                      : Icons.account_circle_outlined,
                   color: Colors.white,
                   size: 110,
                 ),
                 const SizedBox(
                   height: 20,
+                ),
+                Visibility(
+                  visible: !entrar,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Nome",
+                          fillColor: Colors.white,
+                          filled: true,
+                          hintStyle: const TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w200,
+                            fontSize: 16,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w200,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
                 ),
                 TextFormField(
                   decoration: InputDecoration(
@@ -91,6 +125,40 @@ class _LoginState extends State<Login> {
                   ),
                   obscureText: true,
                 ),
+                Visibility(
+                  visible: !entrar,
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Confirme a senha",
+                          fillColor: Colors.white,
+                          filled: true,
+                          hintStyle: const TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w200,
+                            fontSize: 16,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w200,
+                          fontSize: 16,
+                        ),
+                        obscureText: true,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                    ],
+                  ),
+                ),
                 Padding(
                   padding: const EdgeInsets.only(top: 15, bottom: 15),
                   child: SizedBox(
@@ -98,14 +166,15 @@ class _LoginState extends State<Login> {
                     child: ElevatedButton(
                       onPressed: () {},
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red,
+                        backgroundColor:
+                            (entrar) ? Colors.red : Colors.deepOrangeAccent,
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(6),
                         ),
                       ),
-                      child: const Text(
-                        "Entrar",
-                        style: TextStyle(
+                      child: Text(
+                        (entrar) ? "Entrar" : "Cadastrar",
+                        style: const TextStyle(
                           color: Colors.white,
                           fontSize: 18,
                         ),
@@ -114,10 +183,14 @@ class _LoginState extends State<Login> {
                   ),
                 ),
                 TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Cadastre-se",
-                    style: TextStyle(
+                  onPressed: () {
+                    setState(() {
+                      entrar = !entrar;
+                    });
+                  },
+                  child: Text(
+                    (entrar) ? "Cadastre-se" : "Entre",
+                    style: const TextStyle(
                       color: Colors.white,
                       fontSize: 18,
                     ),
